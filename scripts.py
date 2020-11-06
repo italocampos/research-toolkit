1fef19f071cc7c64793c7e9a572cf97a12fe7be1
Italo Campos <italo.ramon.campos@gmail.com>
2020-11-06 19:24:44 -0300

    Adds functions to show details of the results

--- scripts.py
+++ scripts.py
@@ -43,7 +43,7 @@ def evaluate_solutions(results):
     Raises
     ------
     Exception
-        In case the solution don't match any supported network model
+        In case the solution don't match any supported network model.
     '''
 
     for i, res in enumerate(results):
@@ -142,7 +142,83 @@ def select_best(results):
 
 
 def show_all_values(results):
-    ''' Show the values of all the solutions in 'results'
+    ''' Shows the most relevant values of the solutions in 'results'
+
+    Parameters
+    ----------
+    results : list
+        A list with the results of the TS. This list is a list of dicts under
+        the following form:
+        {
+            'time': float, # The time wasted in the search
+            'best': list # The best solution found in the search,
+            'total_i': int, # The number of the final iteration 
+            'i_best': int, # The iteration where the best solution was found
+            'fault': list, # The list with the faulted lines
+            'i_local': int, # The parameter of the max local search
+            'itm': int, # The parameter of the max iteration to reset the TS
+            'max_i': int, # The parameter of the max iteration set in the TS
+        }
+    
+    Raises
+    ------
+    Exception
+        In case the solution don't match any supported network model.
+    '''
+
+    print(color.yellow('EXECUTION;NAB;NRB;VALUE;TIME (S);OPENED SW;CLOSED SW;FAULT LINES'))
+    for i, res in enumerate(results):
+        if len(res['best']) == len(default_topologies['10-bus']):
+            default = default_topologies['10-bus'].copy()
+        elif len(res['best']) == len(default_topologies['16-bus']):
+            default = default_topologies['16-bus'].copy()
+        elif len(res['best']) == len(default_topologies['33-bus']):
+            default = default_topologies['33-bus'].copy()
+        elif len(res['best']) == len(default_topologies['119-bus']):
+            default = default_topologies['119-bus'].copy()
+        else:
+            raise(Exception('No such model to this solution.'))
+        
+        faulted = default.copy()
+        for f in res['fault']:
+            faulted[f] = 0
+
+        value = ev.value(res['best'])
+
+        zeros = list()
+        for j, element in enumerate(default):
+            if element == 0:
+                zeros.append(j)
+        
+        ones = list()
+        for j, element in enumerate(default):
+            if element == 1:
+                ones.append(j)
+        
+        opened_switches = list()
+        for j, element in enumerate(res['best']):
+            if element == 0 and j not in zeros:
+                opened_switches.append(j)
+        
+        closed_switches = list()
+        for j, element in enumerate(res['best']):
+            if element == 1 and j not in ones:
+                closed_switches.append(j)
+        
+        print('{execution};{nab};{nrb};{value};{time};{opened_sw};{closed_sw};{fault_lines}'.format(
+            execution = i,
+            nab = len(ev.unsupplied_buses(faulted)),
+            nrb = value - (ev.value(default) - len(ev.unsupplied_buses(faulted))),
+            value = value,
+            time = res['time'],
+            opened_sw = opened_switches,
+            closed_sw =  closed_switches,
+            fault_lines = res['fault'],
+            ))
+
+
+def show_times(results):
+    ''' Shows the respective values of time for all the solutions in 'results'
 
     Parameters
     ----------
@@ -160,5 +236,106 @@ def show_all_values(results):
             'max_i': int, # The parameter of the max iteration set in the TS
         }
     '''
-    for i, r in enumerate(results):
-        print('Solution: %d; value: %d' % (i, ev.value(r['best'])))
+
+    for r in results:
+        print(r['time'])
+
+
+def show_opened_lines(results):
+    ''' Shows the lines that were opened in each solution
+
+    Parameters
+    ----------
+    results : list
+        A list with the results of the TS. This list is a list of dicts under
+        the following form:
+        {
+            'time': float, # The time wasted in the search
+            'best': list # The best solution found in the search,
+            'total_i': int, # The number of the final iteration 
+            'i_best': int, # The iteration where the best solution was found
+            'fault': list, # The list with the faulted lines
+            'i_local': int, # The parameter of the max local search
+            'itm': int, # The parameter of the max iteration to reset the TS
+            'max_i': int, # The parameter of the max iteration set in the TS
+        }
+    
+    Raises
+    ------
+    Exception
+        In case the solution don't match any supported network model.
+    '''
+
+    for i, res in enumerate(results):
+        if len(res['best']) == len(default_topologies['10-bus']):
+            default = default_topologies['10-bus'].copy()
+        elif len(res['best']) == len(default_topologies['16-bus']):
+            default = default_topologies['16-bus'].copy()
+        elif len(res['best']) == len(default_topologies['33-bus']):
+            default = default_topologies['33-bus'].copy()
+        elif len(res['best']) == len(default_topologies['119-bus']):
+            default = default_topologies['119-bus'].copy()
+        else:
+            raise(Exception('No such model to this solution.'))
+
+        zeros = list()
+        for j, element in enumerate(default):
+            if element == 0:
+                zeros.append(j)
+            
+        opened_switches = list()
+        for j, element in enumerate(res['best']):
+            if element == 0 and j not in zeros:
+                opened_switches.append(j)
+            
+        print(opened_switches)
+
+
+def show_closed_lines(results):
+    ''' Shows the lines that were closed in each solution
+
+    Parameters
+    ----------
+    results : list
+        A list with the results of the TS. This list is a list of dicts under
+        the following form:
+        {
+            'time': float, # The time wasted in the search
+            'best': list # The best solution found in the search,
+            'total_i': int, # The number of the final iteration 
+            'i_best': int, # The iteration where the best solution was found
+            'fault': list, # The list with the faulted lines
+            'i_local': int, # The parameter of the max local search
+            'itm': int, # The parameter of the max iteration to reset the TS
+            'max_i': int, # The parameter of the max iteration set in the TS
+        }
+    
+    Raises
+    ------
+    Exception
+        In case the solution don't match any supported network model.
+    '''
+
+    for i, res in enumerate(results):
+        if len(res['best']) == len(default_topologies['10-bus']):
+            default = default_topologies['10-bus'].copy()
+        elif len(res['best']) == len(default_topologies['16-bus']):
+            default = default_topologies['16-bus'].copy()
+        elif len(res['best']) == len(default_topologies['33-bus']):
+            default = default_topologies['33-bus'].copy()
+        elif len(res['best']) == len(default_topologies['119-bus']):
+            default = default_topologies['119-bus'].copy()
+        else:
+            raise(Exception('No such model to this solution.'))
+        
+        ones = list()
+        for j, element in enumerate(default):
+            if element == 1:
+                ones.append(j)
+        
+        closed_switches = list()
+        for j, element in enumerate(res['best']):
+            if element == 1 and j not in ones:
+                closed_switches.append(j)
+            
+        print(closed_switches)
